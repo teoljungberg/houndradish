40feecafe3ee22e9da8aa174a57f31af4b59322e
Teo Ljungberg <teo@teoljungberg.com>
2015-10-28 09:26:27 +0100

    Violate long lines

--- app/assets/javascripts/es6.js
+++ app/assets/javascripts/es6.js
@@ -13,3 +13,9 @@ class Dog extends Animal {
     console.log(this.name + ' barks.');
   }
 }
+
+class Cat extends Animal {
+  speak() {
+    console.log(this.name + " loves to chase cards and bite their tires like a dog, but isn't one.");
+  }
+}
