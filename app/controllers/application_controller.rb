391067e20df851a6cc97f71ffb05160a3c800b38
Teo Ljungberg <teo@teoljungberg.com>
2015-12-04 11:12:38 +0100

    Disabled Style/StringLiterals
    Should result in no violations

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,9 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def some_action
+    # rubocop:disable Style/StringLiterals
+    'hello world'
+  end
 end
